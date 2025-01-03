1733d62c639eb940c199c59212dbf3fb7eb77955
Yoann <ygrim@cfai-formation.fr>
2025-01-03 17:26:55 +0100

    Ajout du tp06

--- deploy/api/controllers/catalogue.controllers.js
+++ deploy/api/controllers/catalogue.controllers.js
@@ -1,8 +1,30 @@
 
 exports.get = (req, res) => {
         const catalogue = [
-		{ref:"X001", titre : "Linux", prix : 10},
-		{ref:"X002", titre : "Angular", prix : 20}
+			{
+				"id": 1,
+				"name": "Product 1",
+				"description": "Product 1 description",
+				"price": 100,
+			},
+			{
+				"id": 2,
+				"name": "Product 2",
+				"description": "Product 2 description",
+				"price": 200,
+			},
+			{
+				"id": 3,
+				"name": "Product 3",
+				"description": "Product 3 description",
+				"price": 300,
+			},
+			{
+				"id": 4,
+				"name": "Product 4",
+				"description": "Product 4 description",
+				"price": 400,
+			}
 		];
 		
 	
