82ba65c99ebdd708aa0506581040fcdb1d21285e
Yoann <ygrim@cfai-formation.fr>
2025-01-10 09:35:06 +0100

    Test du deploiement de la base

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -1,12 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-cu0d8p9u0jms73cvfhjg-a.oregon-postgres.render.com",
+    "host" :"dpg-cu0dm31u0jms73cvmidg-a.frankfurt-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_kait_user",
-    "password" : "N65kixW5IYmQYemT69oNaSCRdSbE5PFQ",
-    "bdname" :"cnam_kait" 
+    "user" : "cnam_db_fgnf_user",
+    "password" : "IwI4WH51at5sxfmrTXzGGOsxxCKVcjZW",
+    "bdname" :"cnam_db_fgnf" 
     }
 }
-
 
